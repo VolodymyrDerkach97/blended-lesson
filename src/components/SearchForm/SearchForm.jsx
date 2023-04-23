afd63a671597d571b2566997f029862f41127910
Oleg <sirov@ukr.net>
2023-04-23 11:09:48 +0300

    form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,27 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  submit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.submit}>
+        <InputSearch
+          value={this.state.query}
+          onChange={e => this.setState({ query: e.target.value })}
+        />
+        <FormBtn>
+          <FiSearch />
+        </FormBtn>
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,18 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+  };
+
+  handleSubmit = q => {
+    this.setState({ query: q });
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.handleSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
