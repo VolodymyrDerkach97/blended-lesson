6f7cdbb31344093f87c5f0fd03f0e69f3f43b6a5
Haidai Kostiantyn <thekos0596@gmail.com>
2023-04-23 11:54:26 +0300

    created api request

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = '563492ad6f9170000100000145a0bdf3467e4f8fbd7fec0694446ed4';
 axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.params = {
@@ -8,4 +8,7 @@ axios.defaults.params = {
   per_page: 15,
 };
 
-export const getImages = (query, page) => {};
+export const getImages = async (query, page) => {
+  const response = await axios.get(`search?query=${query}&page=${page}`);
+  return response.data;
+};
